9b949bb43a182a4b27c24c0a990d606773f21558
Scott Gottreu <scott.gottreu@warrendouglas.com>
2014-08-21 17:29:45 -0500

    A few more UI fixes.

--- public/javascripts/builder.js
+++ public/javascripts/builder.js
@@ -12,26 +12,66 @@
     var tabIndex = 0;
     var numPlayers = $("#numPlayers").val();
 
+    html += '<div class="left scoring_labels">';
     $.each(gameData.scoring, function(i, value) {
-      html += '<div><span class="'+value.slug+'_label">'+value.title+'</span>';
-
-      for(var x =0; x < numPlayers;x++) {
-        iType = (i >= 1) ? 'tel' : 'text';
-        tabIndex = parseFloat(numPlayers)+4+(parseFloat(numPlayers) *x);
-
-        html += '<span class="'+value.slug+x+'">';
-        html += '<input type="'+iType+'" class="col'+x+'" tabIndex="'+tabIndex+'" data-cols="'+x+'" id="'+value.slug+x+'" name="'+value.slug+'"></span>';
-      }
-      html += "</div>";
+        html += '<span class="'+value.slug+'_label scoring_label">'+value.title+'</span>';
     });
+    html += '<span class="scoring_label"></span></div>';
 
-    html += '<div><span></span>';
-    for(var x =0; x < $("#numPlayers").val();x++) {
-      html += '<span class="instance point_totals" id="points'+x+'"></span>';
+    for(var x =0; x < numPlayers;x++) {
+      html += '<div class="left playercol player'+x+'">';
+
+      $.each(gameData.scoring, function(i, value) {
+        html += '<span class="player_score_wrapper">';
+
+        html += '<span class="hidden '+value.slug+'_label">'+value.title+'</span>';
+
+        //for(var x =0; x < numPlayers;x++) {
+          iType = (i >= 1) ? 'tel' : 'text';
+          tabIndex = parseFloat(numPlayers)+4+(parseFloat(numPlayers) *x);
+          
+          html += '<span class="'+value.slug+x+' txt_field">';
+          html += '<input type="'+iType+'" class="col'+x+'" tabIndex="'+tabIndex+'" data-cols="'+x+'" id="'+value.slug+x+'" name="'+value.slug+'"></span>';
+          html
+          
+          html += '</span>';
+        
+      });
+      html += '<span class="instance point_totals player_score_wrapper" id="points'+x+'"></span>';
+
+      html += '<span class="instance buttons player_score_wrapper" >';
+      if(x > 0) {
+        html += '<button class="prev" data-cols="'+x+'">Prev</button>';
+      }
+      if(x < numPlayers-1) {
+        html += '<button class="next" data-cols="'+x+'">Next</button>';
+      }
+      html += '</span>';
+      html += "</div>";
     }
-    html += "</div>";
+
+    // html += '<div>';
+    // for(var x =0; x < $("#numPlayers").val();x++) {
+    //   html += '<span class="instance point_totals" id="points'+x+'"></span>';
+    // }
+    // html += "</div>";
 
     $("#points").html(html);
+
+    $(".next").on({
+      click: function() {
+        var x = parseFloat($(this).data('cols'))+1;
+        $(".playercol").hide();
+        $(".player"+x).show();
+      }
+    });
+    $(".prev").on({
+      click: function() {
+        var x = parseFloat($(this).data('cols'))-1;
+        $(".playercol").hide();
+        $(".player"+x).show();
+      }
+    });
 
     for(x =0; x < $("#numPlayers").val();x++) {
       $(".col"+x).on({
@@ -39,11 +79,16 @@
           var total = 0, step = 0;
 
           $.each($("."+$(this)[0].className), function(i, val) {
-            step = getPoints($("#"+this.id).val(),i);
+            step = getPoints(this.value,i);
             total += step;
           });
           var x = $(this).data('cols');
           $("#points"+x).html(total);
+        },
+        focus: function() {
+          $(".playercol").hide();
+          var x = $(this).data('cols');
+          $(".player"+x).show();
         }
       });
     }
